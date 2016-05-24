dbe9517423fcbec82ae1e9e38a7af204bf03a336
c41x <jakubduracz@gmail.com>
2016-05-24 19:04:00 +0200

    + config, initialize / create "things" table

--- configure.php
+++ configure.php
@@ -1,19 +1,41 @@
 <?php
 
 function addLine($s) {
-	echo $s.'<br />';
+    echo $s.'<br />';
 }
 
-function tableExists($tableName) {
-	return mysqli_query('select 1 from `'.mysqli_real_escape_string($tableName).'` limit 1') !== FALSE;
+function tableExists($db, $tableName) {
+    return $db->query('SELECT 1 FROM `'.$tableName.'` LIMIT 1') !== FALSE;
 }
 
-if (mysqli_connect("localhost", "root", "pass", "everything") == FALSE) {
-	addLine("could not connect to server.");
+$db = mysqli_connect("localhost", "root", "pass", "everything");
+
+if ($db->connect_errno) {
+    addLine("could not connect to server.");
 }
 else {
-	addLine("connection to MySQL database established.");
-	// TODO: check for tables, if there are none - query create one
+    addLine("connection to MySQL database established.");
+
+    if (tableExists($db, 'things')) {
+	addLine("table things - ok");
+	// TODO: verify?
+    }
+    else {
+	addLine("creating things table...");
+	if ($db->query("CREATE TABLE things (".
+		       "id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,".
+		       "name_id VARCHAR(255) NOT NULL,".
+		       "pretty_name VARCHAR(255) NOT NULL,".
+		       "html TEXT,".
+		       "js TEXT,".
+		       "css TEXT".
+		       ")") == FALSE) {
+	    addLine("error creating things table");
+	}
+	else {
+	    addLine("table things created");
+	}
+    }
 }
 
 ?>
