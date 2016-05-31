9e372ea3cc9eb8e2c9d08a4b02b9e4ab68c321de
c41x <jakubduracz@gmail.com>
2016-05-31 18:55:53 +0200

    " -> '

--- configure.php
+++ configure.php
@@ -11,112 +11,111 @@ function tableExists($db, $tableName) {
 function initializeDatabse($db) {
     // things
     if (tableExists($db, 'things')) {
-	addLine("table things - ok");
+	addLine('table things - ok');
     }
     else {
-	addLine("creating things table...");
-	if ($db->query("CREATE TABLE things (".
-		       "id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,".
-		       "name_id VARCHAR(255)".$charsetPostfix."NOT NULL,".
-		       "pretty_name VARCHAR(255)".$charsetPostfix."NOT NULL,".
-		       "html TEXT".$charsetPostfix.",".
-		       "js TEXT".$charsetPostfix.",".
-		       "css TEXT".$charsetPostfix.
-		       ")") == FALSE) {
-	    addLine("error creating things table");
+	addLine('creating things table...');
+	if ($db->query('CREATE TABLE things ('.
+		       'id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,'.
+		       'name_id VARCHAR(255)'.$charsetPostfix.'NOT NULL,'.
+		       'pretty_name VARCHAR(255)'.$charsetPostfix.'NOT NULL,'.
+		       'html TEXT'.$charsetPostfix.','.
+		       'js TEXT'.$charsetPostfix.','.
+		       'css TEXT'.$charsetPostfix.
+		       ')') == FALSE) {
+	    addLine('error creating things table');
 	}
 	else {
-	    addLine("table things created");
+	    addLine('table things created');
 	}
     }
 
     // nodes
     if (tableExists($db, 'nodes')) {
-	addLine("table nodes - ok");
+	addLine('table nodes - ok');
     }
     else {
-	addLine("creating nodes table...");
-	if ($db->query("CREATE TABLE nodes (".
-		       "id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,".
-		       "id_things INT UNSIGNED NOT NULL, ".
-		       "state TEXT".$charsetPostfix.
-		       ")") == FALSE) {
-	    addLine("error creating things nodes");
+	addLine('creating nodes table...');
+	if ($db->query('CREATE TABLE nodes ('.
+		       'id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,'.
+		       'id_things INT UNSIGNED NOT NULL, '.
+		       'state TEXT'.$charsetPostfix.
+		       ')') == FALSE) {
+	    addLine('error creating things nodes');
 	}
 	else {
-	    addLine("table nodes created");
+	    addLine('table nodes created');
 	}
     }
 
     // pages
     if (tableExists($db, 'pages')) {
-	addLine("table pages - ok");
+	addLine('table pages - ok');
     }
     else {
-	addLine("creating pages table...");
-	if ($db->query("CREATE TABLE pages (".
-		       "id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, ".
-		       "title VARCHAR(255)".$charsetPostfix.
-		       ")") == FALSE) {
-	    addLine("error creating pages table");
+	addLine('creating pages table...');
+	if ($db->query('CREATE TABLE pages ('.
+		       'id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, '.
+		       'title VARCHAR(255)'.$charsetPostfix.
+		       ')') == FALSE) {
+	    addLine('error creating pages table');
 	}
 	else {
-	    addLine("table pages created");
+	    addLine('table pages created');
 	}
     }
 
     // page_nodes
     if (tableExists($db, 'page_nodes')) {
-	addLine("table page_nodes - ok");
+	addLine('table page_nodes - ok');
     }
     else {
-	addLine("creating page_nodes table...");
-	if ($db->query("CREATE TABLE page_nodes (".
-		       "id_page INT UNSIGNED NOT NULL, ".
-		       "id_node INT UNSIGNED NOT NULL".
-		       ")") == FALSE) {
-	    addLine("error creating page_nodes table");
+	addLine('creating page_nodes table...');
+	if ($db->query('CREATE TABLE page_nodes ('.
+		       'id_page INT UNSIGNED NOT NULL, '.
+		       'id_node INT UNSIGNED NOT NULL'.
+		       ')') == FALSE) {
+	    addLine('error creating page_nodes table');
 	}
 	else {
-	    addLine("table page_nodes created");
+	    addLine('table page_nodes created');
 	}
     }
 }
 
 $charsetPostfix = ' CHARACTER SET utf8 COLLATE utf8_bin ';
-$db = mysqli_connect("localhost", "root", "pass", "everything");
+$db = mysqli_connect('localhost', 'root', 'pass', 'everything');
 
 if ($db->connect_errno !== 0) {
-    addLine("could not connect to server");
+    addLine('could not connect to server');
 }
 else {
-    addLine("connection to MySQL database established.");
+    addLine('connection to MySQL database established.');
     if ($_GET) {
-	if (isset($_GET["install"]) && $_POST && isset($_POST["name_id"]) && isset($_POST["pretty_name"]) &&
-	    isset($_POST["html"]) && isset($_POST["js"]) && isset($_POST["css"])) {
-	    if ($db->query("INSERT INTO things (name_id, pretty_name, html, js, css) VALUES (".
-			   "'".mysqli_escape_string($db, $_POST['name_id'])."', ".
-			   "'".mysqli_escape_string($db, $_POST['pretty_name'])."', ".
-			   "'".mysqli_escape_string($db, $_POST['html'])."', ".
-			   "'".mysqli_escape_string($db, $_POST['js'])."', ".
-			   "'".mysqli_escape_string($db, $_POST['css'])."'".
-			   ")") == FALSE) {
-		addLine("could not install thing");
+	if (isset($_GET['install']) && $_POST && isset($_POST['name_id']) && isset($_POST['pretty_name']) &&
+	    isset($_POST['html']) && isset($_POST['js']) && isset($_POST['css'])) {
+	    if ($db->query('INSERT INTO things (name_id, pretty_name, html, js, css) VALUES ('.
+			   '\''.mysqli_escape_string($db, $_POST['name_id']).'\', '.
+			   '\''.mysqli_escape_string($db, $_POST['pretty_name']).'\', '.
+			   '\''.mysqli_escape_string($db, $_POST['html']).'\', '.
+			   '\''.mysqli_escape_string($db, $_POST['js']).'\', '.
+			   '\''.mysqli_escape_string($db, $_POST['css']).'\''.
+			   ')') == FALSE) {
+		addLine('could not install thing');
 	    }
 	    else {
-		addLine("thing \"".$_POST["pretty_name"]."\" installed");
+		addLine('thing "'.$_POST['pretty_name'].'" installed');
 	    }
 	}
 	else {
-	    addLine("could not install thing: POST data incomplete");
+	    addLine('could not install thing: POST data incomplete');
 	}
     }
     else {
 	initializeDatabse($db);
     }
-    addLine("done");
+    addLine('done');
 }
 
 // TODO: verify tables structure
-// TODO: " -> '
 ?>
