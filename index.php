e58bf398be1fa9e8d2715365b97bbc9646a228d8
c41x <jakubduracz@gmail.com>
2016-06-01 18:28:17 +0200

    + generate js code for thing

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
+<?php
+require 'config.php';
+$db = mysqli_connect($host, $user, $password, $database);
+$thingsResult = $db->query('SELECT * FROM things');
+$things = array();
+$i = 0;
+if ($thingsResult->num_rows > 0) {
+    while ($row = $thingsResult->fetch_assoc()) {
+	$things[$i++] = $row;
+    }
+}
+?>
 <!DOCTYPE html>
 <meta charset="utf-8">
 <title>Everything</title>
 <style>
-
- .draggable { width: 150px; height: 150px; padding: 0.5em; }
  body { margin: 0; }
-
+ <?php
+ foreach ($things as &$thing) {
+     echo $thing['css'];
+ }
+ ?>
 </style>
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
@@ -13,92 +27,31 @@
 <body>
   <script>
    $(function() {
-       /*
-	  user specified content all stored in DB
-	  as JS part. all functions must follow naming
-	  convention
-	*/
-       var serializeDraggable = function(id) {
-	   // 1) gets state as JSON
-	   // 2) sends update request to server, print error if disconnected
-	   // or 2) push data to update queue?
-	   var obj = $(id);
-	   var dataToSend = JSON.stringify({"top" : obj.offset().top,
-					    "left" : obj.offset().left});
-	   $.ajax({
-	       type: "POST",
-	       url: "set-node.php?id=" + id,
-	       data: dataToSend,
-	       contentType: "application/json",
-	       success: function(data) {
-		   alert("data send to server: " + dataToSend + " response: " + data);
-	       },
-	       error: function(a, b, c) {
-		   alert("sending error: " + dataToSend);
-		   alert(b);
-		   alert(c);
-	       }
-	   });
-       };
-
-       var deserializeDraggable = function(id) {
-	   // 1) sends request for state
-	   // 2) apply state
+       var genericSpawn = function(type, setupFunction) {
 	   $.ajax({
 	       dataType: "json",
-	       url: "get-node.php?id=" + id,
-	       success: function(data) {
-		   $("#" + id).offset(data);
-		   alert("loaded from server: " + JSON.stringify(data));
-	       },
-	       error: function(a, b, c) {
-		   alert(b);
-		   alert(c);
-	       }
-	   });
-       };
-
-       var setupDraggable = function(id) {
-	   // main JS logic on id - initialize all
-	   $(id).draggable({
-               drag: function() {
-		   var offset = $(this).offset();
-		   var xPos = offset.left;
-		   var yPos = offset.top;
-		   $('#posX').text('x: ' + xPos);
-		   $('#posY').text('y: ' + yPos);
-               },
-               stop: function() {
-		   // just tests
-		   serializeDraggable(id);
-		   //removeDraggable(id);
-               }
-	   });
-       };
-
-       /*
-	  automatically generated for each thing type
-	*/
-       var spawnDraggable = function() {
-	   // 1) name is generated from "spawn" + <thing_name>
-	   // 2) sends AJAX request with thing id e.g.: create.php?thing=draggable
-	   // 3) server returns JSON with html: "html code" and id: "#id"
-	   // 4) code is calling "setup" + <thing_name> function for JS setup part
-	   // TODO: generic
-	   $.ajax({
-	       dataType: "json",
-	       url: "create-node.php?type=draggable",
+	       url: "create-node.php?type=" + type,
 	       success: function(data) {
 		   alert("spawning: " + data.html);
 		   $(document.body).append(data.html);
-		   setupDraggable("#" + data.id);
+		   setupFunction("#" + data.id);
 	       },
 	       error: function(a, b, c) {
 		   alert(b);
 		   alert(c);
 	       }
 	   });
-       };
+       }
+
+       <?php
+       foreach ($things as &$thing) {
+	   echo $thing['js'];
+	   echo 'var spawn'.$thing['name_id'].' = function() { genericSpawn("'.
+		$thing['name_id'].'", setup'.$thing['name_id'].'); }';
+	   // TODO: remove from server
+	   //echo 'remove'.$thing['name_id'].' = function(id) { $id.remove(); }';
+       }
+       ?>
 
        var removeDraggable = function(id) {
 	   // 1) remove from DOM
