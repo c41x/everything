66c5bfe6f4f524d75c6f0278ec16af1ed46a565f
c41x <jakubduracz@gmail.com>
2016-07-21 18:29:30 +0200

    + admin panel

---
+++ admin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>Thing Editor</title>
+<head>
+  <style>
+   body {
+     font-family: "Helvetica Neue", Helvetica, Arial;
+     font-size: 14px;
+     line-height: 20px;
+     font-weight: 400;
+     color: #3b3b3b;
+     -webkit-font-smoothing: antialiased;
+     font-smoothing: antialiased;
+     background: #2b2b2b;
+     padding: 100px;
+   }
+   a {
+     color: red;
+   }
+  </style>
+</head>
+
+<body>
+  <p><a href="configure.php">Install (configure database)</a></p>
+  <p><a href="install-thing.php">Install Thing</a></p>
+  <p><a href="edit-things.php">Edit Things</a></p>
+</body>
